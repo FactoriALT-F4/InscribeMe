6fb99f741e1d38b151bd4c3bdba341423c5f5430
Francisco Manuel <fsaa93@gmail.com>
2023-07-27 15:28:49 +0200

    All entities done

---
+++ backend/inscribemeapp/src/main/java/f5/inscribeme/models/Events.java
@@ -0,0 +1,57 @@
+package f5.inscribeme.models;
+
+import java.sql.Date;
+// import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "events")
+@Data
+public class Events {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long idevents;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "description")
+    public String description;
+
+    @Column(name = "starting_date")
+    public Date startingDate;
+
+    @Column(name = "end_date")
+    public Date enDate;
+
+    // @Column(name = "creation_date")
+    // public Timestamp creationDate;
+
+    @Column(name="creation_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+
+    @Column(name = "max_people")
+    public Long maxPeople;
+
+    @Column(name = "imgUrl")
+    public String imgUrl;
+
+    @OneToMany(mappedBy="event")
+    public List<ProfileEvents> profileEvent;
+    
+
+}
--- backend/inscribemeapp/src/main/java/f5/inscribeme/models/User.java
+++ backend/inscribemeapp/src/main/java/f5/inscribeme/models/User.java
@@ -10,27 +10,33 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @Data
 public class User {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long iduser;
 
-    @Column(name="mail")
+    @Column(name = "mail")
     public String mail;
 
-    @Column(name="password")
+    @Column(name = "password")
     public String password;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name="permission_id", nullable = false)
+    @JoinColumn(name = "permission_id", nullable = false)
     @JsonIgnore
     private UserType userType;
+
+    @OneToOne
+    @JoinColumn(name = "profile_id", nullable = false)
+    @JsonIgnore
+    private Profile profile;;
 
 }
