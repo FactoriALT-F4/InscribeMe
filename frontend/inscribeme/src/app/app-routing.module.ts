7fa24836c33b4e37f6f03e6e2c195c010c802de4
NataliaGM1 <ngmihalache@gmail.com>
2023-07-28 16:22:48 +0200

    formulary html done

--- frontend/inscribeme/src/app/app-routing.module.ts
+++ frontend/inscribeme/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginformComponent } from './events/loginform/loginform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path : 'loginform', component : LoginformComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
