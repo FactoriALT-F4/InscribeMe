a3426ad7bec539a54d1fbd2d646f034575adbbbd
snake324 <kristi4n90@gmail.com>
2023-07-28 14:27:03 +0200

    all components added

---
+++ frontend/inscribeme/src/app/service/events.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Events } from '../events/models/events.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+
+  constructor() { }
+}
