51d074e69fda2abf5595ace08649bd4231327c86
snake324 <kristi4n90@gmail.com>
2023-07-28 14:50:34 +0200

    user service added

---
+++ frontend/inscribeme/src/app/service/events/events.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Events } from 'src/app/events/models/events.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+
+  constructor() { }
+}
