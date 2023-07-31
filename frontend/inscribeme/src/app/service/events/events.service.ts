1ae6d306c38f2eee50fc7afff03c96fd6467f3be
snake324 <kristi4n90@gmail.com>
2023-07-31 14:09:26 +0200

    slider + api fake service

--- frontend/inscribeme/src/app/service/events/events.service.ts
+++ frontend/inscribeme/src/app/service/events/events.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Events } from 'src/app/events/models/events.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) { }
+
+  getSliderData(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
