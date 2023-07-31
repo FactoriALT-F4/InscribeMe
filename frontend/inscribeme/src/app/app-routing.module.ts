638e0283a03f41fdc15c8f9ec68b7c5149803c29
NataliaGM1 <ngmihalache@gmail.com>
2023-07-31 16:29:35 +0200

    route path

--- frontend/inscribeme/src/app/app-routing.module.ts
+++ frontend/inscribeme/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { RegisterComponent } from './login/views/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: '', component: RegisterComponent }
+    
 ];
 
 @NgModule({
