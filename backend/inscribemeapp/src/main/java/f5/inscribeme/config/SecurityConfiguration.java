4b650f9f2cc2841f83f613999274f241de10a848
Francisco Manuel <fsaa93@gmail.com>
2023-08-02 08:54:08 +0200

    SecurityUser and configuration done but no tested

--- backend/inscribemeapp/src/main/java/f5/inscribeme/config/SecurityConfiguration.java
+++ backend/inscribemeapp/src/main/java/f5/inscribeme/config/SecurityConfiguration.java
@@ -2,27 +2,28 @@ package f5.inscribeme.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import f5.inscribeme.services.JpaUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    
+    JpaUserDetailsService jpaUserDetailsService;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -34,12 +35,18 @@ public class SecurityConfiguration {
                         .logoutUrl("/logout")
                         .deleteCookies("JSESSIONID"))
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/events/**","/users/register").permitAll())
+                        .requestMatchers("/events/**", "/users/register").permitAll())
+                .userDetailsService(jpaUserDetailsService)
                 .httpBasic(withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
 
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -52,28 +59,5 @@ public class SecurityConfiguration {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
-    // @Bean
-    // public InMemoryUserDetailsManager userDetailsManager() {
-
-    //     UserDetails donald = User.builder()
-    //             .username("donald")
-    //             .password("{bcrypt}$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO")
-    //             .roles("USER")
-    //             .build();
-
-    //     UserDetails pepa = User.builder()
-    //             .username("pepa")
-    //             .password("{bcrypt}$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO")
-    //             .roles("ADMIN")
-    //             .build();
-
-    //     Collection<UserDetails> users = new ArrayList<>();
-    //             users.add(donald);
-    //             users.add(pepa);
-
-    //     return new InMemoryUserDetailsManager(users);
-    // }
-
 
 }
