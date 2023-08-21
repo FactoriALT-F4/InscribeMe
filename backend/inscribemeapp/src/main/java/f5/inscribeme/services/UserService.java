b96be5965007880fecf7d9263e91000140a78183
Francisco Manuel <fsaa93@gmail.com>
2023-08-21 15:49:44 +0200

    some tests TODO security user

--- backend/inscribemeapp/src/main/java/f5/inscribeme/services/UserService.java
+++ backend/inscribemeapp/src/main/java/f5/inscribeme/services/UserService.java
@@ -2,8 +2,8 @@ package f5.inscribeme.services;
 
 import java.util.*;
 
-import f5.inscribeme.models.Profile;
-import jakarta.annotation.PostConstruct;
+// import f5.inscribeme.models.Profile;
+// import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,7 +14,7 @@ import f5.inscribeme.repositories.UserRepo;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     UserRepo repo;
     @Autowired
@@ -22,14 +22,15 @@ public class UserService {
     @Autowired
     ProfileService profileService;
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return repo.findAll();
     }
-    public User getUserById(Long id){
+
+    public User getUserById(Long id) {
         Optional<User> opt = repo.findById(id);
-        if(opt.isPresent()){
+        if (opt.isPresent()) {
             return opt.get();
-        }else{
+        } else {
             return null;
         }
     }
@@ -50,46 +51,46 @@ public class UserService {
         }
     }
 
-    public void assignDefaultUserType(User user) throws Throwable{
+    public void assignDefaultUserType(User user) throws Throwable {
         UserType defaultType = utService.show(1L);
         Set<UserType> userTypes = new HashSet<>();
         userTypes.add(defaultType);
 
-        user.setUserTypes(userTypes); //Esto lo agregue yo
+        user.setUserTypes(userTypes); // Esto lo agregue yo
     }
 
-    public User updateUser(User user){
+    public User updateUser(User user) {
         return repo.save(user);
     }
-    public void deleteUser(User user){
+
+    public void deleteUser(User user) {
         repo.delete(user);
     }
 
+    // @PostConstruct
+    // public void insertInitialUsers() {
+    //     BCryptPasswordEncoder pwEncoder = new BCryptPasswordEncoder();
 
-    @PostConstruct
-    public void insertInitialUsers() {
-        BCryptPasswordEncoder pwEncoder = new BCryptPasswordEncoder();
+    //     Profile profileAdmin = new Profile();
+    //     profileAdmin.setName("Admin");
+    //     profileAdmin.setLastname("Inscribeme");
+    //     profileService.addProfile(profileAdmin);
 
-        Profile profileAdmin = new Profile();
-        profileAdmin.setName("Admin");
-        profileAdmin.setLastname("Inscribeme");
-        profileService.addProfile(profileAdmin);
+    //     Profile profileUser = new Profile();
+    //     profileUser.setName("User");
+    //     profileUser.setLastname("Inscribeme");
+    //     profileService.addProfile(profileUser);
 
-        Profile profileUser = new Profile();
-        profileUser.setName("User");
-        profileUser.setLastname("Inscribeme");
-        profileService.addProfile(profileUser);
+    //     User userAdmin = new User();
+    //     userAdmin.setMail("admin@inscribeme.com");
+    //     userAdmin.setPassword(pwEncoder.encode("password1"));
+    //     userAdmin.setProfile(profileAdmin);
+    //     addUser(userAdmin);
 
-        User userAdmin = new User();
-        userAdmin.setMail("admin@inscribeme.com");
-        userAdmin.setPassword(pwEncoder.encode("password1"));
-        userAdmin.setProfile(profileAdmin);
-        addUser(userAdmin);
-
-        User userUser = new User();
-        userUser.setMail("user@inscribeme.com");
-        userUser.setPassword(pwEncoder.encode("password2"));
-        userUser.setProfile(profileUser);
-        addUser(userUser);
-    }
+    //     User userUser = new User();
+    //     userUser.setMail("user@inscribeme.com");
+    //     userUser.setPassword(pwEncoder.encode("password2"));
+    //     userUser.setProfile(profileUser);
+    //     addUser(userUser);
+    // }
 }
