703df7d5ec2c767848aefec987d3f6b51e727671
Francisco Manuel <fsaa93@gmail.com>
2023-08-29 12:28:58 +0200

    Update data.sql with USER type user

--- backend/inscribemeapp/src/main/resources/data.sql
+++ backend/inscribemeapp/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 /* users */
 INSERT INTO users (iduser, mail, password) VALUES
-(1, 'admin@inscribeme.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
+(1, 'admin@inscribeme.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO'),
+(2, 'user@inscribeme.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
 
 /* permissions */
 INSERT INTO permissions (id_user_type, type) VALUES
@@ -9,7 +10,9 @@ INSERT INTO permissions (id_user_type, type) VALUES
 
 /* user_profile */
 INSERT INTO user_profile (idprofile, lastname, name) VALUES
-(1, 'Admin', 'Admin');
+(1, 'Admin', 'Admin'),
+(2, 'User', 'User');
 
 INSERT INTO permissions_users (user_id, user_type_id) VALUES
-(1, 1);
+(1, 1),
+(2, 2);
