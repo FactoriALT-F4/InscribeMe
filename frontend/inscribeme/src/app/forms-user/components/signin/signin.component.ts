c8d0a4c18ff71b2e07e4a28944b3f05c5baa4ed1
DirtdiverIV <davidgomez.seg@gmail.com>
2023-08-30 16:10:01 +0200

    Tried user authentication

--- frontend/inscribeme/src/app/forms-user/components/signin/signin.component.ts
+++ frontend/inscribeme/src/app/forms-user/components/signin/signin.component.ts
@@ -1,7 +1,30 @@
+// import { Component } from '@angular/core';
+// import { UsersService } from '../../service/users/users.service';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-signin',
+//   templateUrl: './signin.component.html',
+//   styleUrls: ['./signin.component.scss']
+// })
+// export class SigninComponent {
+//   email: string = ''; 
+//   password: string = '';
+
+//   constructor(public userService: UsersService, private router: Router) {}
+
+//   login() {
+//     const user = { email: this.email, password: this.password };
+//     this.userService.login(user).subscribe((data) => {
+//       this.userService.setToken(data.token);
+//      
+//     });
+//   }
+// }
+
 import { Component } from '@angular/core';
-import { UsersService } from "../../service/users/users.service";
+import { UsersService } from '../../service/users/users.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -9,19 +32,34 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent {
-  mail!: string;
-  password!: string;
- 
+  credentials = {
+    mail: '',
+    password: ''
+  };
 
-  constructor(public userService: UsersService, private router: Router) {}
-
+  constructor(private userService: UsersService, private router: Router) {}
   
-  login() {
-    const user = { mail: this.mail, password: this.password };
-    this.userService.login(user).subscribe((data) => {
-      this.userService.setToken(data.token);
-      this.router.navigateByUrl("/");
-    });
+  users: any[] = [];
+  
+  ngOnInit() {
+    this.userService.getUsers().subscribe(
+      (data) => {
+        this.users = data;
+        console.log('Usuarios:', this.users);
+      },
+      (error) => {
+        console.error('Error al obtener usuarios:', error);
+      }
+    );
   }
 
+
+
+  login() {
+    this.userService.authenticate(this.credentials, () => {
+      this.router.navigateByUrl("/");
+      console.log('Usuario autenticado:', this.userService.authenticated);
+    });
+  }
 }
+
--- frontend/inscribeme/src/app/forms-user/service/users/users.service.ts
+++ frontend/inscribeme/src/app/forms-user/service/users/users.service.ts
@@ -3,56 +3,66 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { CookieService } from "ngx-cookie-service";
 
+interface ServerResponse {
+  name?: string; 
+  
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  // authenticated = false;
+  authenticated = false;
 
   constructor(private http: HttpClient, private cookies: CookieService) { }
 
 
-  // authenticate(credentials: { mail: string; password: string; }, callback: () => any) {
+  
 
-  //   const headers = new HttpHeaders(credentials ? {
-  //     authorization: 'Basic ' + btoa(credentials.mail + ':' + credentials.password)
-  //   } : {});
+  authenticate(credentials: { mail: string; password: string; }, callback: () => any) {
 
-  //   this.http.get('user', { headers: headers }).subscribe(response => {
-  //     if (response['mail']) {
-  //       this.authenticated = true;
-  //     } else {
-  //       this.authenticated = false;
-  //     }
-  //     return callback && callback();
-  //   });
+    const headers = new HttpHeaders(credentials ? {
+      authorization: 'Basic ' + btoa(credentials.mail + ':' + credentials.password)
+    } : {});
 
+    this.http.get<ServerResponse>('manolo', { headers }).subscribe(response => {
+      if (response.name) {
+        this.authenticated = true;
+      } else {
+        this.authenticated = false;
+      }
+      return callback && callback();
+    });
+  }
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost:4000/users');
+  }
+
+  // login(user: any): Observable<any> {
+  //   return this.http.get("http://localhost:4000/login", user);
   // }
 
+  // setToken(token: string) {
+  //   this.cookies.set("token", token);
+  // }
 
-  login(user: any): Observable<any> {
-    return this.http.get("http://localhost:4000/login", user);
-  }
+  // getToken() {
+  //   return this.cookies.get("token");
+  // }
 
-  setToken(token: string) {
-    this.cookies.set("token", token);
-  }
+  // getUser() {
+  //   return this.http.get("http://localhost:4000/users");
+  // }
 
-  getToken() {
-    return this.cookies.get("token");
-  }
+  // getUserLogged(): Observable<any> {
+  //   const token = this.getToken();
+  //   const headers = new HttpHeaders({
+  //     Authorization: `Bearer ${token}`
+  //   });
 
-  getUser() {
-    return this.http.get("http://localhost:4000/users");
-  }
-
-  getUserLogged(): Observable<any> {
-    const token = this.getToken();
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`
-    });
-
-    return this.http.get("http://localhost:4000/users", { headers });
-  }
+  //   return this.http.get("http://localhost:4000/users", { headers });
+  // }
 }
